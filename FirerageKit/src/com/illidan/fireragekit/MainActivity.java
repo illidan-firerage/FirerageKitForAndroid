0bfcfaf7dd39d9751a21926a1e009689e8739530
illidan <illidan.firerage@qq.com>
2014-12-24 18:14:08 +0800

    slidingmenu

--- FirerageKit/src/com/illidan/fireragekit/MainActivity.java
+++ FirerageKit/src/com/illidan/fireragekit/MainActivity.java
@@ -1,16 +1,49 @@
 package com.illidan.fireragekit;
 
+import android.app.ActionBar.LayoutParams;
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MainActivity extends Activity {
+	
+	private static Context mContext;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+//		setContentView(R.layout.activity_main);
+		
+		mContext = this;
+		
+		setTitle(R.string.app_name);
+		
+		// configure the SlidingMenu
+
+		LinearLayout layout = new LinearLayout(this); 
+	    layout.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
+
+	    Button button = new Button(this);
+	    button.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT,LayoutParams.WRAP_CONTENT));
+	    button.setText("Back");
+	    layout.addView(button);
+	    setContentView(layout);
+	    button.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent(mContext, FragmentChangeActivity.class);
+				startActivity(intent);
+			}
+		});
+	    
 	}
 
 	@Override
